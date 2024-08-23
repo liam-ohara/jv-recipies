045b84a113bb31bd78c43271001ca278608e2365
Liam O'Hara <liam.ohara@gmail.com>
2024-08-23 11:04:15 +0100

    Break: partial implementation of getSessionFactory.

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -1,10 +1,23 @@
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import static java.lang.Boolean.TRUE;
+import static org.hibernate.cfg.JdbcSettings.*;
 
 public class Database {
 
     public static SessionFactory getSessionFactory(){
-        // TODO
+        var sessionFactory = new Configuration()
+                // use H2 in-memory database
+                .setProperty(URL, "jdbc:h2:mem:db1")
+                // default username / password
+                .setProperty(USER, "sa")
+                .setProperty(PASS, "")
+                // options to display SQL in console
+                .setProperty(SHOW_SQL, TRUE.toString())
+                .setProperty(FORMAT_SQL, TRUE.toString())
+                .setProperty(HIGHLIGHT_SQL, TRUE.toString())
+                .buildSessionFactory();
     }
 
 }
