39bab5ba25cbdafdbe93c1e6de1c6b3b0d2b1ac8
Liam O'Hara <liam.ohara@gmail.com>
2024-08-23 11:15:02 +0100

    Feat: add main class with skeleton to export schema.

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import org.hibernate.SessionFactory;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try (SessionFactory sessionFactory = Database.getSessionFactory()) {
+            sessionFactory.getSchemaManager().exportMappedObjects(true);
+            sessionFactory.close();
+        }
+    }
+}
